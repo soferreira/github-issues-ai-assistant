d267426af1c81668df2b817c36b5e58ddb0240ab
Saverio Proto <saverioproto@microsoft.com>
2023-06-28 17:26:11 +0200

    Adding some openai

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.10.11-bullseye
 ADD . /app
 WORKDIR /app
 
-RUN pip install langchain PyGithub cffi cryptography
+RUN pip install langchain openai PyGithub cffi cryptography
 
 ENV PYTHONPATH /app
 CMD ["python3", "/app/main.py"]
--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import os
 from github import Github
 from github import Auth
+import openai
+
+
+def get_completion(prompt, model="gpt-35-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        engine="gpt-35-turbo",
+        messages=messages,
+        temperature=0,
+    )
+    return response.choices[0].message["content"]
 
 
 def main():
+    openai.api_type = "azure"
+    openai.api_key = os.environ["INPUT_OPENAI_API_KEY"]
+    openai.api_base = os.environ["INPUT_OPENAI_API_BASE"]
+    openai.api_version = "2023-05-15"
     issuenumber = os.environ["INPUT_ISSUE_NUMBER"]
     print(f'Processing: {issuenumber}')
+    print(f'baseurl: {openai.api_base}')
+
     auth = Auth.Token(os.environ["INPUT_REPO-TOKEN"])
     g = Github(auth=auth)
     repo = g.get_repo("zioproto/github-issues-ai-assistant")
     issue = repo.get_issue(number=int(issuenumber))
-    issue.create_comment("Test")
+    # Create a langchain agent:
+
+    prompt = f"""Make a short summary of the following GitHub issue:\
+          ```{issue.body}```"""
+    response = get_completion(prompt)
+    issue.create_comment(response)
 
 
 if __name__ == "__main__":
