503d5e43c86fc44e66cb902ffa912bb410e346b6
Saverio Proto <saverioproto@microsoft.com>
2023-08-07 17:21:17 +0200

    Refactory to use just an agent

--- main.py
+++ main.py
@@ -9,68 +9,54 @@ from langchain.chat_models import AzureChatOpenAI
 from dotenv import load_dotenv
 from github import Github, Auth
 
+from langchain.llms.openai import AzureOpenAI
+from langchain.chat_models import AzureChatOpenAI
+from langchain.agents import load_tools, initialize_agent, AgentType
+
+# Uncomment to debug:
+#import langchain
+#langchain.debug = True
+
+
 from prompt_templates import prompt_infos, MULTI_PROMPT_ROUTER_TEMPLATE
-from issue_examples import DEFAULT_ISSUE_BODY
+from issue_examples import DEFAULT_ISSUE_BODY, DEFAULT_BUG_BODY
 
+bingurl = "https://api.bing.microsoft.com/v7.0/search"
 
-def langchain_router_chain(input_prompt):
+def runagent(issue_body):
+
+    simpleprompt = PromptTemplate.from_template(
+        """
+        I will provide you a Github Issue opened in a repository of Terraform modules for Azure. You have to write a comment on the issue. The comment should be one of the following:
+        1. If the issue is reporting a bug, you can analyse its description and check if all the information to reproduce the bug is present. If not, you can ask the reporter to provide more information.
+        2. If the issue is requesting a new feature for the Terraform module, you can analyse the issue description and tell if the corresponding Azure feature is in preview or not.
+
+        Here is the description: ```{issue_body}```
+
+        Your output must be only the GitHub comment as it will appear on GitHub. Sign your comment with the name GitHub AI Issue Assistant.
+        """
+    )
+
     llm = AzureChatOpenAI(
         openai_api_base=os.environ["INPUT_OPENAI_API_BASE"],
-        openai_api_version="2023-05-15",
-        deployment_name="gpt-35-turbo",
+        openai_api_version="2023-07-01-preview",
+        deployment_name="gpt-35-turbo-16k",
         openai_api_key=os.environ["INPUT_OPENAI_API_KEY"],
         openai_api_type="azure",
         temperature=0,
     )
 
-    destination_chains = {}
+    bing_key = os.environ["INPUT_BING_SUBSCRIPTION_KEY"]
 
-    # Create a list that holds the destination chains
-    for p_info in prompt_infos:
-        prompt = ChatPromptTemplate.from_template(
-            template=p_info["prompt_template"]
-        )
-        chain = LLMChain(llm=llm, prompt=prompt)
-        destination_chains[p_info["name"]] = chain
+    tools = load_tools(["bing-search"],llm,bing_subscription_key=bing_key,bing_search_url=bingurl)
 
-    # Create a list of destination names and descriptions
-    destinations = [f"{p['name']}: {p['description']}" for p in prompt_infos]
-    print(f'{destinations = }')
+    agent = initialize_agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=False)
 
-    # Create a string with the destination names and descriptions all together
-    # in a single string separated by newlines
-    destinations_str = "\n".join(destinations)
+    #issue_body = issue_body.replace("```", "")  # temporary fix, remove backticks that confuse the model
 
-    # Create a prompt that is a just a empty string with the {input} placeholder
-    default_prompt = ChatPromptTemplate.from_template("Make a summary of this issue:\n {input}")
+    result = agent.run(simpleprompt.format(issue_body=issue_body))
+    return result
 
-    default_chain = LLMChain(llm=llm, prompt=default_prompt)
-
-    router_template = MULTI_PROMPT_ROUTER_TEMPLATE.format(
-        destinations=destinations_str
-    )
-
-    router_prompt = PromptTemplate(
-        template=router_template,
-        input_variables=["input"],
-        output_parser=RouterOutputParser(),
-    )
-
-    router_chain = LLMRouterChain.from_llm(llm, router_prompt)
-
-    multi_prompt_chain = MultiPromptChain(
-        router_chain=router_chain,
-        destination_chains=destination_chains,
-        default_chain=default_chain,
-        verbose=True,
-    )
-
-    #print(f'{input_prompt = }')
-    input_prompt = input_prompt.replace("```", "")  # temporary fix, remove backticks that confuse the model
-    response = multi_prompt_chain.run(input_prompt)
-    print(f'{response = }')
-
-    return response
 
 
 def run_github_action():
@@ -85,7 +71,7 @@ def run_github_action():
     repo = github.get_repo(repository)
     issue = repo.get_issue(number=int(issue_number))
 
-    response = langchain_router_chain(issue.body)
+    response = runagent(issue.body)
     issue.create_comment(response)
 
 
@@ -95,7 +81,13 @@ def run_locally():
     load_dotenv()
 
     # Run the chain locally with default issue body
-    response = langchain_router_chain(DEFAULT_ISSUE_BODY)
+    print("Running locally with default issue body")
+    response = runagent(DEFAULT_ISSUE_BODY)
+    print(response)
+
+    # Run the chain locally with default bug body
+    print("Running locally with default bug body")
+    response = runagent(DEFAULT_BUG_BODY)
     print(response)
 
 
